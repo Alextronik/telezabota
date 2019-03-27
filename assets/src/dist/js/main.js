c750fcaef6e477aa4a30709b583d603bd2196664
MaksimLavrenyuk <maksim.lavrenyuk@outlook.com>
2019-03-27 23:31:02 +0300

    index

--- assets/src/dist/js/main.js
+++ assets/src/dist/js/main.js
@@ -9,6 +9,8 @@
 //= ../../../../node_modules/bootstrap/js/dist/util.js
 // модальное окно
 //= ../../../../node_modules/bootstrap/js/dist/modal.js
+// коллапс
+//= ../../../../node_modules/bootstrap/js/dist/collapse.js
 
 // Импортируем телефонную валидацию
 //= ../../../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
